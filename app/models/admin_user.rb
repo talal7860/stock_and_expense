b1a55897711751d19b9f132a7907170a5582da7c
Talal Arshad <talal7860@gmail.com>
2017-02-23 16:35:01 +0500

    adding more relations

--- app/models/admin_user.rb
+++ app/models/admin_user.rb
@@ -3,9 +3,9 @@ class AdminUser < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable,
          :recoverable, :rememberable, :trackable, :validatable, :lockable
-  has_many :expenses
-  has_many :company_wallets
-  has_many :stock_ins
-  has_many :stock_outs
-  has_many :company_transactions
+  has_many :added_expenses, foreign_key: :added_by_id
+  has_many :company_wallets, foreign_key: :added_by_id
+  has_many :stock_ins, foreign_key: :added_by_id
+  has_many :stock_outs, foreign_key: :added_by_id
+  has_many :company_transactions, foreign_key: :added_by_id
 end
--- app/models/stock_out.rb
+++ app/models/stock_out.rb
@@ -1,2 +1,3 @@
 class StockOut < ApplicationRecord
+  belongs_to :added_by, foreign_key: :added_by_id, class: "AdminUser"
 end
